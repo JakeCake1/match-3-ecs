208f8f7283d4c1eb8f9b4e6e556caab2d6baeb2b
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-11-07 14:10:01 +0500

    Add features to game

--- Assets/Scripts/EcsStartup.cs
+++ Assets/Scripts/EcsStartup.cs
@@ -1,50 +1,69 @@
+using Data;
 using Leopotam.EcsLite;
+using Systems.Camera;
+using Systems.Grid;
 using UnityEngine;
+using VContainer;
+using Views;
 
-namespace Client {
-    sealed class EcsStartup : MonoBehaviour {
-        EcsWorld _world;        
-        IEcsSystems _systems;
+internal sealed class EcsStartup : MonoBehaviour
+{
+  private EcsWorld _world;
+  private IEcsSystems _systems;
+  
+  private CellView _cellViewPrefab;
+  private FieldData _fieldData;
+  
+  private CameraData _cameraData;
+  private Camera _camera;
 
-        void Start () {
-            _world = new EcsWorld ();
-            _systems = new EcsSystems (_world);
-            _systems
-                // register your systems here, for example:
-                // .Add (new TestSystem1 ())
-                // .Add (new TestSystem2 ())
-                
-                // register additional worlds here, for example:
-                // .AddWorld (new EcsWorld (), "events")
+  [Inject]
+  public void Construct(CellView cellViewPrefab, FieldData fieldData, Camera camera, CameraData cameraData)
+  {
+    _cameraData = cameraData;
+    _camera = camera;
+    _fieldData = fieldData;
+    _cellViewPrefab = cellViewPrefab;
+  }
+
+  private void Start()
+  {
+    _world = new EcsWorld();
+    _systems = new EcsSystems(_world);
+    _systems
+      .Add (new CreateFieldSystem(_fieldData))
+      .Add (new CreateCellViewSystem(_cellViewPrefab, _fieldData))
+      .Add (new CameraResizeSystem(_camera, _cameraData))
 #if UNITY_EDITOR
-                // add debug systems for custom worlds here, for example:
-                // .Add (new Leopotam.EcsLite.UnityEditor.EcsWorldDebugSystem ("events"))
-                .Add (new Leopotam.EcsLite.UnityEditor.EcsWorldDebugSystem ())
+      .Add(new Leopotam.EcsLite.UnityEditor.EcsWorldDebugSystem())
 #endif
-                .Init ();
-        }
+      .Init();
+  }
 
-        void Update () {
-            // process systems here.
-            _systems?.Run ();
-        }
+  private void Update() => 
+    _systems?.Run();
 
-        void OnDestroy () {
-            if (_systems != null) {
-                // list of custom worlds will be cleared
-                // during IEcsSystems.Destroy(). so, you
-                // need to save it here if you need.
-                _systems.Destroy ();
-                _systems = null;
-            }
-            
-            // cleanup custom worlds here.
-            
-            // cleanup default world.
-            if (_world != null) {
-                _world.Destroy ();
-                _world = null;
-            }
-        }
+  private void OnDestroy()
+  {
+    CleanupSystems();
+    CleanupWorlds();
+  }
+
+  private void CleanupSystems()
+  {
+    if (_systems != null)
+    {
+      _systems.Destroy();
+      _systems = null;
     }
+  }
+
+  private void CleanupWorlds()
+  {
+    if (_world != null)
+    {
+      _world.Destroy();
+      _world = null;
+    }
+  }
 }
