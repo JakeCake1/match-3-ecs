647dbeb8402a13b03a48b9081bea1a9a6544afd1
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-11-08 15:57:44 +0500

    Refactoring destroy chips system

--- Assets/Scripts/Systems/Chips/DestroyChipsSystem.cs
+++ Assets/Scripts/Systems/Chips/DestroyChipsSystem.cs
@@ -15,9 +15,7 @@ namespace Systems.Chips
     private EcsPool<Chip> _chipsPool;
     private EcsPool<BusyCell> _busyCellsPool;
     
-    private EcsPool<GridPosition> _gridPool;
     private EcsPool<ChipViewRef> _chipViewsPool;
-    private EcsPool<ChipForDestroy> _chipsForDestroyPool;
 
     public void Init(IEcsSystems systems)
     {
@@ -28,9 +26,7 @@ namespace Systems.Chips
       _chipsPool = _world.GetPool<Chip>();
       _busyCellsPool = _world.GetPool<BusyCell>();
       
-      _gridPool = _world.GetPool<GridPosition>();
       _chipViewsPool = _world.GetPool<ChipViewRef>();
-      _chipsForDestroyPool = _world.GetPool<ChipForDestroy>();
       
       Debug.Log($"Init: {GetType().Name}");
     }
@@ -41,16 +37,11 @@ namespace Systems.Chips
         return;
       
       foreach (int chipEntity in _chipsForDestroyFilter)
-      {
-        _chipsForDestroyPool.Del(chipEntity);
-          
+      {       
         _chipViewsPool.Get(chipEntity).ChipView.Destroy();
-        _chipViewsPool.Del(chipEntity);
+        _busyCellsPool.Del(_chipsPool.Get(chipEntity).ParentCellEntityIndex);
         
-        _gridPool.Del(chipEntity);     
-        
-        _busyCellsPool.Del(_chipsPool.Get(chipEntity).ParentCellEnitiyIndex);
-        _chipsPool.Del(chipEntity);
+        _world.DelEntity(chipEntity);
       }
       
       Debug.Log($"Run: {GetType().Name}");
