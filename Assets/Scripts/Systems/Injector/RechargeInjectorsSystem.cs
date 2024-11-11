fa31a24bbbc56d5613869471eaecf7e94e65ab74
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-11-11 21:43:01 +0500

    Refactoring RechargeInjectorsSystem.cs

--- Assets/Scripts/Systems/Injector/RechargeInjectorsSystem.cs
+++ Assets/Scripts/Systems/Injector/RechargeInjectorsSystem.cs
@@ -1,64 +1,52 @@
 using Components.Cell;
 using Components.Cell.Markers;
-using Components.Chips;
 using Components.Common;
 using Components.Injector;
 using Components.Injector.Markers;
 using Leopotam.EcsLite;
-using UnityEngine;
 
 namespace Systems.Injector
 {
-  public class RechargeInjectorsSystem : IEcsInitSystem, IEcsRunSystem
+  public sealed class RechargeInjectorsSystem : IEcsInitSystem, IEcsRunSystem
   {
     private EcsWorld _world;
 
-    private EcsFilter _filterInjectors;
-    private EcsFilter _filterCells;
+    private EcsFilter _notReadyInjectorsFilter;
+    private EcsFilter _freeCellsFilter;
 
-    private EcsPool<BusyCellComponent> _busyPool;
-    private EcsPool<ReadyInjectorComponent> _readyPool;
-    
-    private EcsPool<GridPositionComponent> _gridPositionPool;
+    private EcsPool<ReadyInjectorComponent> _readyInjectorsPool;
+    private EcsPool<GridPositionComponent> _gridPositionsPool;
 
     public void Init(IEcsSystems systems)
     {
       _world = systems.GetWorld();
 
-      _filterInjectors = _world.Filter<ChipsInjectorComponent>().Exc<ReadyInjectorComponent>().End();
-      _filterCells = _world.Filter<CellComponent>().Inc<GridPositionComponent>().Exc<BusyCellComponent>().End();
+      _notReadyInjectorsFilter = _world.Filter<ChipsInjectorComponent>().Exc<ReadyInjectorComponent>().End();
+      _freeCellsFilter = _world.Filter<CellComponent>().Inc<GridPositionComponent>().Exc<BusyCellComponent>().End();
 
-      _gridPositionPool = _world.GetPool<GridPositionComponent>();
-      _busyPool = _world.GetPool<BusyCellComponent>();
-      _readyPool = _world.GetPool<ReadyInjectorComponent>();
-
-      Debug.Log($"Init: {GetType().Name}");
+      _readyInjectorsPool = _world.GetPool<ReadyInjectorComponent>();
+      _gridPositionsPool = _world.GetPool<GridPositionComponent>();
     }
 
     public void Run(IEcsSystems systems)
     {
-      if (_filterCells.GetEntitiesCount() == 0)
+      if (AllCellsAreBusy())
         return;
 
-      foreach (int cellsEntityIndex in _filterCells)
+      foreach (int cellEntityIndex in _freeCellsFilter)
       {
-        if (_busyPool.Has(cellsEntityIndex))
-          continue;
-
-        foreach (int injectorEntityIndex in _filterInjectors)
+        foreach (int injectorEntityIndex in _notReadyInjectorsFilter)
         {
-          if (_gridPositionPool.Get(injectorEntityIndex).Position.x == _gridPositionPool.Get(cellsEntityIndex).Position.x)
-          {
-            if (_readyPool.Has(injectorEntityIndex))
-              continue;
-            
-            _readyPool.Add(injectorEntityIndex);
-            break;
-          }
+          if (_gridPositionsPool.Get(injectorEntityIndex).Position.x != _gridPositionsPool.Get(cellEntityIndex).Position.x)
+            continue;
+          
+          _readyInjectorsPool.Add(injectorEntityIndex);
+          break;
         }
       }
 
-      Debug.Log($"Run: {GetType().Name}");
+      bool AllCellsAreBusy() => 
+        _freeCellsFilter.GetEntitiesCount() == 0;
     }
   }
 }
