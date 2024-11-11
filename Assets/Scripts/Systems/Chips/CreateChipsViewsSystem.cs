ae876427a69bfc182a51676310ba788f68c61572
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-11-11 20:39:23 +0500

    Refactoring CreateChipsViewsSystem.cs

--- Assets/Scripts/Systems/Chips/CreateChipsViewsSystem.cs
+++ Assets/Scripts/Systems/Chips/CreateChipsViewsSystem.cs
@@ -7,66 +7,74 @@ using Views;
 
 namespace Systems.Chips
 {
-  public class CreateChipsViewsSystem : IEcsInitSystem, IEcsRunSystem
+  public sealed class CreateChipsViewsSystem : IEcsInitSystem, IEcsRunSystem, IEcsDestroySystem
   {
     private readonly ChipView _chipViewPrefab;
     private readonly FieldData _fieldData;
-    
-    private EcsWorld _world;
-    
-    private EcsFilter _filterChips;
-    
+
+    private EcsFilter _chipsWithoutViews;
+
     private EcsPool<ChipComponent> _chipsPool;
-    private EcsPool<GridPositionComponent> _gridPositionPool;
-    private EcsPool<ChipViewRefComponent> _chipViewRefPool;
-    
+    private EcsPool<GridPositionComponent> _gridPositionsPool;
+    private EcsPool<ChipViewRefComponent> _chipViewRefsPool;
+
     private Transform _chipsParent;
 
     public CreateChipsViewsSystem(FieldData fieldData, ChipView chipViewPrefab)
     {
-      _fieldData = fieldData;
       _chipViewPrefab = chipViewPrefab;
+      _fieldData = fieldData;
     }
 
     public void Init(IEcsSystems systems)
     {
-      _chipsParent = new GameObject("Chips").transform;
-      
-      _world = systems.GetWorld();
+      var world = systems.GetWorld();
 
-      _filterChips = _world.Filter<ChipComponent>().Exc<ChipViewRefComponent>().End();
-      
-      _chipsPool = _world.GetPool<ChipComponent>();
-      _gridPositionPool = _world.GetPool<GridPositionComponent>();
-      
-      _chipViewRefPool = _world.GetPool<ChipViewRefComponent>();
-      
-      Debug.Log($"Init: {GetType().Name}");
+      _chipsWithoutViews = world.Filter<ChipComponent>().Exc<ChipViewRefComponent>().End();
+
+      _chipsPool = world.GetPool<ChipComponent>();
+      _gridPositionsPool = world.GetPool<GridPositionComponent>();
+      _chipViewRefsPool = world.GetPool<ChipViewRefComponent>();
+
+      CreateChipsParentObject();
     }
 
     public void Run(IEcsSystems systems)
-    {    
-      if (_filterChips.GetEntitiesCount() == 0)
+    {
+      if (AllChipsHaveViews())
         return;
-      
-      foreach (int chipEntity in _filterChips)
-      {
-        ref ChipViewRefComponent chipViewRef = ref _chipViewRefPool.Add(chipEntity);
-        ref GridPositionComponent gridPosition = ref _gridPositionPool.Get(chipEntity);
-        ref ChipComponent chip = ref _chipsPool.Get(chipEntity);
-        
-        chip.EntityIndex = chipEntity;
-        
-        var chipView = Object.Instantiate(_chipViewPrefab, _chipsParent).GetComponent<ChipView>();
-        
-        chipView.Construct(chipEntity, _fieldData.Offset);
-        chipView.SetPosition(gridPosition.Position);
-        chipView.SetType(chip.Type);
-        
-        chipViewRef.ChipView = chipView;
-      }
-      
-      Debug.Log($"Run: {GetType().Name}");
+
+      foreach (int chipEntity in _chipsWithoutViews) 
+        CreateChipView(chipEntity);
+
+      bool AllChipsHaveViews() => 
+        _chipsWithoutViews.GetEntitiesCount() == 0;
     }
+
+    public void Destroy(IEcsSystems systems) => 
+      DestroyChipsParentObject();
+
+    private void CreateChipView(int chipEntity)
+    {
+      ref ChipViewRefComponent chipViewRef = ref _chipViewRefsPool.Add(chipEntity);
+      ref GridPositionComponent gridPosition = ref _gridPositionsPool.Get(chipEntity);
+      ref ChipComponent chip = ref _chipsPool.Get(chipEntity);
+
+      chip.EntityIndex = chipEntity;
+
+      var chipView = Object.Instantiate(_chipViewPrefab, _chipsParent).GetComponent<ChipView>();
+
+      chipView.Construct(chipEntity, _fieldData.Offset);
+      chipView.SetPosition(gridPosition.Position);
+      chipView.SetType(chip.Type);
+
+      chipViewRef.ChipView = chipView;
+    }
+
+    private void CreateChipsParentObject() =>
+      _chipsParent = new GameObject("Chips").transform;
+
+    private void DestroyChipsParentObject() =>
+      Object.Destroy(_chipsParent);
   }
 }
