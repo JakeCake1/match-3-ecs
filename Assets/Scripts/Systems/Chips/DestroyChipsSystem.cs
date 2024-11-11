49e809a5e00e64ee704f0d5b7a3d2a8ece3c6a39
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-11-11 21:46:06 +0500

    Refactoring DestroyChipsSystem.cs

--- Assets/Scripts/Systems/Chips/DestroyChipsSystem.cs
+++ Assets/Scripts/Systems/Chips/DestroyChipsSystem.cs
@@ -1,14 +1,12 @@
-using Components.Cell;
 using Components.Cell.Markers;
 using Components.Chips;
 using Components.Chips.Markers;
 using Components.Common;
 using Leopotam.EcsLite;
-using UnityEngine;
 
 namespace Systems.Chips
 {
-  public class DestroyChipsSystem : IEcsInitSystem, IEcsRunSystem, IEcsDestroySystem
+  public sealed class DestroyChipsSystem : IEcsInitSystem, IEcsRunSystem
   {
     private EcsWorld _world;
     
@@ -16,40 +14,37 @@ namespace Systems.Chips
     
     private EcsPool<ChipComponent> _chipsPool;
     private EcsPool<BusyCellComponent> _busyCellsPool;
-    
     private EcsPool<ChipViewRefComponent> _chipViewsPool;
 
     public void Init(IEcsSystems systems)
     {
       _world = systems.GetWorld();
 
-      _chipsForDestroyFilter = _world.Filter<ChipComponent>().Inc<GridPositionComponent>().Inc<ChipViewRefComponent>().Inc<ChipForDestroyComponent>().End();
+      _chipsForDestroyFilter = _world.Filter<ChipComponent>()
+        .Inc<GridPositionComponent>()
+        .Inc<ChipViewRefComponent>()
+        .Inc<ChipForDestroyComponent>()
+        .End();
 
       _chipsPool = _world.GetPool<ChipComponent>();
       _busyCellsPool = _world.GetPool<BusyCellComponent>();
-      
       _chipViewsPool = _world.GetPool<ChipViewRefComponent>();
-      
-      Debug.Log($"Init: {GetType().Name}");
     }
 
     public void Run(IEcsSystems systems)
     {
-      if(_chipsForDestroyFilter.GetEntitiesCount() == 0)
+      if(NothingToDestroy())
         return;
       
-      foreach (int chipEntity in _chipsForDestroyFilter)
+      foreach (int chipEntityIndex in _chipsForDestroyFilter)
       {       
-        _chipViewsPool.Get(chipEntity).ChipView.Destroy();
-        _busyCellsPool.Del(_chipsPool.Get(chipEntity).ParentCellEntityIndex);
-        
-        _world.DelEntity(chipEntity);
+        _chipViewsPool.Get(chipEntityIndex).ChipView.Destroy();
+        _busyCellsPool.Del(_chipsPool.Get(chipEntityIndex).ParentCellEntityIndex);
+        _world.DelEntity(chipEntityIndex);
       }
-      
-      Debug.Log($"Run: {GetType().Name}");
-    }
 
-    public void Destroy(IEcsSystems systems) => 
-      Debug.Log($"Destroy: {GetType().Name}");
+      bool NothingToDestroy() => 
+        _chipsForDestroyFilter.GetEntitiesCount() == 0;
+    }
   }
 }
