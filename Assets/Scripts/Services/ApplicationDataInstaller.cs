38303eb53d0c0cd3c0ae98c3d1c3b209db218d6a
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-11-11 18:52:38 +0500

    Refactoring ApplicationLifetimeScope.cs

--- Assets/Scripts/Services/ApplicationDataInstaller.cs
+++ Assets/Scripts/Services/ApplicationDataInstaller.cs
@@ -6,27 +6,31 @@ using Views;
 
 namespace Services
 {
-  public class ApplicationLifetimeScope : LifetimeScope
+  public sealed class ApplicationDataInstaller : LifetimeScope
   {
-    [SerializeField] private CellView _cellViewPrefab;
     [SerializeField] private FieldData _fieldData;
-    [SerializeField] private Camera _camera;
-    [SerializeField] private CameraData _cameraData;
     [SerializeField] private ChipInjectorsData _chipInjectorsData;
+    
+    [SerializeField] private CellView _cellViewPrefab;
     [SerializeField] private ChipView _chipViewPrefab;
     [SerializeField] private ScoreView _scoreViewPrefab;
     [SerializeField] private AddScoreView _addScoreViewPrefab;
     
+    [SerializeField] private Camera _camera;
+    [SerializeField] private CameraData _cameraData;
+
     protected override void Configure(IContainerBuilder builder)
-    {      
-      builder.RegisterInstance(_cellViewPrefab);
+    {
       builder.RegisterInstance(_fieldData);
-      builder.RegisterInstance(_camera);
-      builder.RegisterInstance(_cameraData);
       builder.RegisterInstance(_chipInjectorsData);
+
+      builder.RegisterInstance(_cellViewPrefab);
       builder.RegisterInstance(_chipViewPrefab);
       builder.RegisterInstance(_scoreViewPrefab);
-      builder.RegisterInstance(_addScoreViewPrefab);
+      builder.RegisterInstance(_addScoreViewPrefab);     
+      
+      builder.RegisterInstance(_camera);
+      builder.RegisterInstance(_cameraData);
     }
   }
 }
