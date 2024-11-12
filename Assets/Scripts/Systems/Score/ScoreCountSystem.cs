204e4b56cc3cab5e09345ea3701a2130dc6047fc
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-11-12 13:43:25 +0500

    Refactoring ScoreCountSystem.cs

--- Assets/Scripts/Systems/Score/ScoreCountSystem.cs
+++ Assets/Scripts/Systems/Score/ScoreCountSystem.cs
@@ -5,26 +5,26 @@ using Leopotam.EcsLite;
 
 namespace Systems.Score
 {
-  public class ScoreCountSystem : IEcsInitSystem, IEcsRunSystem
+  public sealed class ScoreCountSystem : IEcsInitSystem, IEcsRunSystem
   {
     private EcsWorld _world;
 
-    private EcsFilter _viewNeedUpdateFilter;
+    private EcsFilter _scoreViewsNeedUpdateFilter;
     
-    private EcsPool<UpdateScoreComponent> _updatePool;
-    private EcsPool<ScoreCountViewRefComponent> _scoreCountViewPool;
+    private EcsPool<UpdateScoreComponent> _needUpdateViewsPool;
+    private EcsPool<ScoreCountViewRefComponent> _scoreCountViewsPool;
     private EcsPool<ScoreCountComponent> _scoreCountPool;
 
-    private int _playerScoreEntity;
+    private int _playerScoreEntityIndex;
 
     public void Init(IEcsSystems systems)
     {
       _world = systems.GetWorld();
 
-      _viewNeedUpdateFilter = _world.Filter<ScoreCountViewRefComponent>().Inc<UpdateScoreComponent>().End();
+      _scoreViewsNeedUpdateFilter = _world.Filter<ScoreCountViewRefComponent>().Inc<UpdateScoreComponent>().End();
       
-      _scoreCountViewPool = _world.GetPool<ScoreCountViewRefComponent>();
-      _updatePool = _world.GetPool<UpdateScoreComponent>();
+      _scoreCountViewsPool = _world.GetPool<ScoreCountViewRefComponent>();
+      _needUpdateViewsPool = _world.GetPool<UpdateScoreComponent>();
       _scoreCountPool = _world.GetPool<ScoreCountComponent>();
 
       CreateScoreCounter(_world);
@@ -32,27 +32,31 @@ namespace Systems.Score
 
     private void CreateScoreCounter(EcsWorld world)
     {
-      _playerScoreEntity = world.NewEntity();
+      _playerScoreEntityIndex = world.NewEntity();
+      
       var scoreCountPool = world.GetPool<ScoreCountComponent>();
         
-      ref ScoreCountComponent scoreCount = ref scoreCountPool.Add(_playerScoreEntity);
+      ref ScoreCountComponent scoreCount = ref scoreCountPool.Add(_playerScoreEntityIndex);
 
-      scoreCount.CounterEntityIndex = _playerScoreEntity;
+      scoreCount.CounterEntityIndex = _playerScoreEntityIndex;
       scoreCount.PlayerScore = 0;
 
-      world.GetPool<NeedCreateScoreCountComponent>().Add(_playerScoreEntity);
+      world.GetPool<NeedCreateScoreCountComponent>().Add(_playerScoreEntityIndex);
     }
 
     public void Run(IEcsSystems systems)
     {
-      foreach (int scoreViewEntityIndex in _viewNeedUpdateFilter)
-      { 
-        ref ScoreCountViewRefComponent scoreCountViewRef = ref _scoreCountViewPool.Get(scoreViewEntityIndex);
-        
-        scoreCountViewRef.ScoreView.SetCount(_scoreCountPool.Get(_playerScoreEntity).PlayerScore);
-        
-        _updatePool.Del(scoreViewEntityIndex);
+      foreach (int scoreViewEntityIndex in _scoreViewsNeedUpdateFilter)
+      {
+        UpdateScoreView(scoreViewEntityIndex);
+        _needUpdateViewsPool.Del(scoreViewEntityIndex);
       }
+    }
+
+    private void UpdateScoreView(int scoreViewEntityIndex)
+    {
+      ref ScoreCountViewRefComponent scoreCountViewRef = ref _scoreCountViewsPool.Get(scoreViewEntityIndex);
+      scoreCountViewRef.ScoreView.SetCount(_scoreCountPool.Get(_playerScoreEntityIndex).PlayerScore);
     }
   }
 }
