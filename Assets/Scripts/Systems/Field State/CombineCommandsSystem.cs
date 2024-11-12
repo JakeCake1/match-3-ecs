e8d515f6e51edcbd27f798ca9aba0b8a93b16347
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-11-12 13:24:27 +0500

    Refactoring CombineCommandsSystem.cs

--- Assets/Scripts/Systems/Field State/CombineCommandsSystem.cs
+++ Assets/Scripts/Systems/Field State/CombineCommandsSystem.cs
@@ -7,14 +7,14 @@ using Leopotam.EcsLite;
 
 namespace Systems.Field_State
 {
-  public class CombineCommandsSystem : IEcsInitSystem, IEcsRunSystem
+  public sealed class CombineCommandsSystem : IEcsInitSystem, IEcsRunSystem
   {
     private EcsWorld _world;
 
     private EcsFilter _emptyMergeCommandsFilter;
 
-    private EcsPool<MergeCommandComponent> _mergeCommandPool;
-    private EcsPool<EmptyCommandComponent> _emptyCommandPool;
+    private EcsPool<MergeCommandComponent> _mergeCommandsPool;
+    private EcsPool<EmptyCommandComponent> _emptyCommandsPool;
 
     public void Init(IEcsSystems systems)
     {
@@ -22,8 +22,8 @@ namespace Systems.Field_State
 
       _emptyMergeCommandsFilter = _world.Filter<MergeCommandComponent>().Inc<EmptyCommandComponent>().End();
 
-      _mergeCommandPool = _world.GetPool<MergeCommandComponent>();
-      _emptyCommandPool = _world.GetPool<EmptyCommandComponent>();
+      _mergeCommandsPool = _world.GetPool<MergeCommandComponent>();
+      _emptyCommandsPool = _world.GetPool<EmptyCommandComponent>();
     }
 
     public void Run(IEcsSystems systems)
@@ -34,13 +34,13 @@ namespace Systems.Field_State
 
     private void OverwriteOverlappingCommands()
     {
-      MergeCommandComponent[] commands = _mergeCommandPool.GetRawDenseItems();
+      MergeCommandComponent[] commands = _mergeCommandsPool.GetRawDenseItems();
       
       for (var i = 0; i < commands.Length; i++)
       {
         for (var j = i + 1; j < commands.Length; j++)
         {
-          if(commands[i].Chips == null || commands[j].Chips == null)
+          if(IsCommandsChipListsNotInitialized(i, j))
             continue;
           
           if (IsTwoCommandHasOverlappedElements(i, j))
@@ -51,6 +51,9 @@ namespace Systems.Field_State
         }
       }
 
+      bool IsCommandsChipListsNotInitialized(int i, int j) =>
+        commands[i].Chips == null || commands[j].Chips == null;
+      
       bool IsTwoCommandHasOverlappedElements(int firstCommandIndex, int secondCommandIndex) => 
         commands[firstCommandIndex].Chips.Intersect(commands[secondCommandIndex].Chips).Any();
 
@@ -63,7 +66,7 @@ namespace Systems.Field_State
       void MarkSecondCommandAsEmpty(int secondCommandIndex)
       {
         commands[secondCommandIndex].Chips.Clear();
-        _emptyCommandPool.Add(commands[secondCommandIndex].CommandEntityIndex);
+        _emptyCommandsPool.Add(commands[secondCommandIndex].CommandEntityIndex);
       }
     }
 
