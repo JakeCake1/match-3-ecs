6d7149d2b0f9f8e3a1525d3cc84f3f911a4a516d
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-11-12 13:39:28 +0500

    Refactoring ReturnNotMergedSystem.cs

--- Assets/Scripts/Systems/Field State/ReturnNotMergedSystem.cs
+++ Assets/Scripts/Systems/Field State/ReturnNotMergedSystem.cs
@@ -5,44 +5,56 @@ using Leopotam.EcsLite;
 
 namespace Systems.Field_State
 {
-  public class ReturnNotMergedSystem : IEcsInitSystem, IEcsRunSystem
+  public sealed class ReturnNotMergedSystem : IEcsInitSystem, IEcsRunSystem
   {
     private EcsWorld _world;
     
-    private EcsFilter _chipFilter;
+    private EcsFilter _chipsInCheckProcessFilter;
     
     private EcsPool<ChipComponent> _chipsPool;
-    private EcsPool<ChipInCheckComponent> _inCheckPool;
-    private EcsPool<SwapCombinationComponent> _swapCombinationPool;
+    private EcsPool<ChipInCheckComponent> _chipsInCheckProcessPool;
+    private EcsPool<SwapCombinationComponent> _swapCombinationsPool;
 
     public void Init(IEcsSystems systems)
     {
       _world = systems.GetWorld();
 
-      _chipFilter = _world.Filter<ChipComponent>().Inc<ChipInCheckComponent>().End();
+      _chipsInCheckProcessFilter = _world.Filter<ChipComponent>().Inc<ChipInCheckComponent>().End();
 
       _chipsPool = _world.GetPool<ChipComponent>();
-      _inCheckPool = _world.GetPool<ChipInCheckComponent>();
-      _swapCombinationPool = _world.GetPool<SwapCombinationComponent>();
+      _chipsInCheckProcessPool = _world.GetPool<ChipInCheckComponent>();
+      _swapCombinationsPool = _world.GetPool<SwapCombinationComponent>();
     }
 
     public void Run(IEcsSystems systems)
     {
-      foreach (int chipEntityIndex in _chipFilter)
+      foreach (int chipEntityIndex in _chipsInCheckProcessFilter)
       {
-        if(!_inCheckPool.Has(chipEntityIndex))
+        if(ChipNotInProcessPool(chipEntityIndex))
           continue;
         
-        ref ChipInCheckComponent chipInCheckChip = ref _inCheckPool.Get(chipEntityIndex);
-
-        int swapCombinationEntity = _world.NewEntity();
-        ref SwapCombinationComponent swapCombination = ref _swapCombinationPool.Add(swapCombinationEntity);
-        
-        swapCombination.Pair = (_chipsPool.Get(chipEntityIndex), _chipsPool.Get(chipInCheckChip.RelatedChip.EntityIndex));
-          
-        _inCheckPool.Del(chipInCheckChip.RelatedChip.EntityIndex);
-        _inCheckPool.Del(chipEntityIndex);
+        CreateSwapCombination(chipEntityIndex);
+        RemoveCheckFlagsForChips(chipEntityIndex);
       }
+      
+      bool ChipNotInProcessPool(int chipEntityIndex) => 
+        !_chipsInCheckProcessPool.Has(chipEntityIndex);
+    }
+
+    private void CreateSwapCombination(int chipEntityIndex)
+    {
+      ref ChipInCheckComponent chipInCheckChip = ref _chipsInCheckProcessPool.Get(chipEntityIndex);
+
+      int swapCombinationEntity = _world.NewEntity();
+      ref SwapCombinationComponent swapCombination = ref _swapCombinationsPool.Add(swapCombinationEntity);
+        
+      swapCombination.Pair = (_chipsPool.Get(chipEntityIndex), _chipsPool.Get(chipInCheckChip.RelatedChip.EntityIndex));
+    }
+
+    private void RemoveCheckFlagsForChips(int chipEntityIndex)
+    {
+      _chipsInCheckProcessPool.Del(_chipsInCheckProcessPool.Get(chipEntityIndex).RelatedChip.EntityIndex);
+      _chipsInCheckProcessPool.Del(chipEntityIndex);
     }
   }
 }
