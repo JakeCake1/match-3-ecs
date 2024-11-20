c72bdea34d60d91f81ee4df922157f42a1320471
Evgenii Popov <evgeny.popov.wooden.toys@gmail.com>
2024-11-20 12:27:52 +0500

    Move addresses to constant values

--- Assets/Scripts/Gameplay/GamePlayInstaller.cs
+++ Assets/Scripts/Gameplay/GamePlayInstaller.cs
@@ -10,8 +10,15 @@ namespace Gameplay
 {
   public class GamePlayInstaller : IInstaller
   {
+    private const string FieldDataAddress = "Field Data";
+    private const string CameraDataAddress = "Camera Data";
+    private const string CellViewAddress = "Cell View";
+    private const string ChipViewAddress = "Chip View";
+    private const string ScoreCountViewAddress = "Score Count View";
+    private const string AddScoreViewAddress = "Add Score View";
+    private const string CameraAddress = "Camera";
+    
     private FieldData _fieldData;
-    private ChipInjectorsData _chipInjectorsData;
 
     private CellView _cellViewPrefab;
     private ChipView _chipViewPrefab;
@@ -28,23 +35,21 @@ namespace Gameplay
 
     public async UniTask Preload()
     {
-      _fieldData = await _assetProvider.Load<FieldData>("Field Data");
-      _chipInjectorsData = await _assetProvider.Load<ChipInjectorsData>("Chip Injector Data");     
-      _cameraData = await _assetProvider.Load<CameraData>("Camera Data");
+      _fieldData = await _assetProvider.Load<FieldData>(FieldDataAddress);
+      _cameraData = await _assetProvider.Load<CameraData>(CameraDataAddress);
 
-      _cellViewPrefab = (await _assetProvider.Load<GameObject>("Cell View")).GetComponentInChildren<CellView>();
-      _chipViewPrefab = (await _assetProvider.Load<GameObject>("Chip View")).GetComponentInChildren<ChipView>();
+      _cellViewPrefab = (await _assetProvider.Load<GameObject>(CellViewAddress)).GetComponentInChildren<CellView>();
+      _chipViewPrefab = (await _assetProvider.Load<GameObject>(ChipViewAddress)).GetComponentInChildren<ChipView>();
       
-      _scoreViewPrefab = (await _assetProvider.Load<GameObject>("Score Count View")).GetComponentInChildren<ScoreView>();
-      _addScoreViewPrefab = (await _assetProvider.Load<GameObject>("Add Score View")).GetComponentInChildren<AddScoreView>();
+      _scoreViewPrefab = (await _assetProvider.Load<GameObject>(ScoreCountViewAddress)).GetComponentInChildren<ScoreView>();
+      _addScoreViewPrefab = (await _assetProvider.Load<GameObject>(AddScoreViewAddress)).GetComponentInChildren<AddScoreView>();
       
-      _camera = (Object.Instantiate(await _assetProvider.Load<GameObject>("Camera"))).GetComponentInChildren<Camera>();
+      _camera = (Object.Instantiate(await _assetProvider.Load<GameObject>(CameraAddress))).GetComponentInChildren<Camera>();
     }
     
     public void Install(IContainerBuilder builder)
     {
       builder.RegisterInstance(_fieldData);
-      builder.RegisterInstance(_chipInjectorsData);
 
       builder.RegisterInstance(_cellViewPrefab);
       builder.RegisterInstance(_chipViewPrefab);
@@ -57,18 +62,17 @@ namespace Gameplay
 
     public void Clear()
     {
-      _assetProvider.Release("Field Data");
-      _assetProvider.Release("Chip Injector Data");     
-      _assetProvider.Release("Camera Data");
+      _assetProvider.Release(FieldDataAddress);
+      _assetProvider.Release(CameraDataAddress);
 
-      _assetProvider.Release("Cell View");
-      _assetProvider.Release("Chip View");
+      _assetProvider.Release(CellViewAddress);
+      _assetProvider.Release(ChipViewAddress);
       
-      _assetProvider.Release("Score Count View");
-      _assetProvider.Release("Add Score View");
+      _assetProvider.Release(ScoreCountViewAddress);
+      _assetProvider.Release(AddScoreViewAddress);
       
       Object.Destroy(_camera.gameObject);
-      _assetProvider.Release("Camera");
+      _assetProvider.Release(CameraAddress);
     }
   }
 }
